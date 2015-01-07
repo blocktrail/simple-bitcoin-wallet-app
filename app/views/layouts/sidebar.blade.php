ff3085a604058c2b4a7072c8ba40495c5dcda8cb
Oisin <oisin@blocktrail.com>
2015-01-07 09:46:49 +0100

    header styling

--- app/views/layouts/sidebar.blade.php
+++ app/views/layouts/sidebar.blade.php
@@ -1,16 +1,19 @@
-<nav class="navbar container">
-    <ul class="navbar-list">
-        <li class="navbar-item"><a class="navbar-link" href="{{URL::route('home')}}">Home</a></li>
+<section class="header">
+    <nav class="navbar container">
+        <ul class="navbar-list">
+            <li class="navbar-item"><a class="navbar-link" href="{{URL::route('home')}}">Home</a></li>
+            <li class="navbar-item"><a class="navbar-link" href="{{URL::route('dashboard')}}">Dashboard</a></li>
 
-        @if(Route::currentRouteName() != 'home')
-        <li class="navbar-item u-pull-right">
-            {{ Form::open(array('route' => 'search', 'method' => 'get')) }}
-                {{ Form::text('query', null, array('placeholder' => 'address, block or transaction', 'class' => 'u-full-width-2')) }}
-                {{ Form::submit('search') }}
-            {{ Form::close() }}
-        </li>
-        @endif
-    </ul>
-    <div>
-    </div>
-</nav>
+            @if(Route::currentRouteName() != 'home')
+            <li class="navbar-item u-pull-right">
+                {{ Form::open(array('route' => 'search', 'method' => 'get')) }}
+                    {{ Form::text('query', null, array('placeholder' => 'address, block or transaction', 'class' => 'u-full-width-2')) }}
+                    {{ Form::submit('search') }}
+                {{ Form::close() }}
+            </li>
+            @endif
+        </ul>
+        <div>
+        </div>
+    </nav>
+</section>
