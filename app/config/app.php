57a24837a001931d44d1a42fa1c82ad7f0341f40
Oisin <oisin@blocktrail.com>
2015-01-07 14:24:57 +0100

    blocktrail service provider

--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'App\Providers\BlocktrailServiceProvider',
+		'App\Providers\Blocktrail\BlocktrailServiceProvider',
 
 	),
 
@@ -189,7 +189,6 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
         'View'              => 'Illuminate\Support\Facades\View',
-        'Blocktrail'        => 'Blocktrail\SDK\BlocktrailSDK',
         'Carbon'            => 'Carbon\Carbon',
 
 	),
