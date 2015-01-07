8b8f6684ac24205c8a181a8eccfff30f304f0d7b
Oisin <oisin@blocktrail.com>
2015-01-07 14:23:26 +0100

    inject blocktrai sdk instance

--- app/controllers/ExplorerController.php
+++ app/controllers/ExplorerController.php
@@ -4,15 +4,8 @@ class ExplorerController extends BaseController {
 
     private $bitcoinClient;
 
-    public function __construct() {
-
-        //initialise the BlocTrail API
-        $apiCredentials = Config::get('services.blocktrail', ['MY_API_KEY','MY_API_SECRET']);
-        $apiKey = $apiCredentials['key'];
-        $apiSecret = $apiCredentials['secret'];
-        $currency = "btc";
-        $this->bitcoinClient = new Blocktrail($apiKey, $apiSecret, $currency, false);
-        $this->bitcoinClient->setCurlDefaultOption('verify', false); //disable ssl verification, for local testing
+    public function __construct(Blocktrail $client) {
+        $this->bitcoinClient = $client;
     }
 
     public function showHome()
