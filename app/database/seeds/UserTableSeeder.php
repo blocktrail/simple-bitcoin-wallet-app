18262981687f02cbf5ce6b0d73371d6af3ef18b6
Oisin <oisin@blocktrail.com>
2015-01-08 17:06:23 +0100

    add webhook auth user

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,6 +6,9 @@ class UserTableSeeder extends Seeder {
     {
         DB::table('users')->delete();
 
+        //create a webhook user for authenticating webhook calls
+        User::create(array('fname' => 'webhook', 'lname' => 'webhook', 'email' => 'webhook@oacdesigns.com', 'password' => 'OxGKbaxYmvCumi'));
+        //testing user
         User::create(array('fname' => 'oisin', 'lname' => 'conolly', 'email' => 'oisin@oacdesigns.com', 'password' => 'test'));
     }
 
