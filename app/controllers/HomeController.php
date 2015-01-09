f77395221b2639ac33a32fc6320c0c6cf2dfa790
Oisin <oisin@blocktrail.com>
2015-01-09 16:49:47 +0100

    fix total balance

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -16,12 +16,12 @@ class HomeController extends BaseController {
 		//lets also add up the balances of all wallets
 		$totalBalance = 0;
 		$totalUncBalance = 0;
-		$wallets->each(function($wallet) use($totalBalance, $totalUncBalance){
+		$wallets->each(function($wallet) use(&$totalBalance, &$totalUncBalance){
 			$wallet->getBalance();
 			$totalBalance += $wallet->balance;
 			$totalUncBalance += $wallet->unc_balance;
 		});
-
+		
 		$data = array(
 			'wallets' => $wallets,
 			'totalBalance' => $totalBalance,
