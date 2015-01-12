84290c4382df6434d21cde3903ce54cc70557aba
Oisin <oisin@blocktrail.com>
2015-01-12 14:42:11 +0100

    add public domain via ngrok tunnel

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -15,4 +15,18 @@ return array(
 
 	'debug' => true,
 
+	/*
+	|--------------------------------------------------------------------------
+	| Application URL
+	|--------------------------------------------------------------------------
+	|
+	| This URL is used by the console to properly generate URLs when using
+	| the Artisan command line tool. You should set this to the root of
+	| your application so that it is used when running Artisan tasks.
+	|
+	*/
+
+	//we're using ngrok (https://ngrok.com/) to create a tunnel from a public domain to our local env
+	'url' => 'http://OxGKbax.ngrok.com',
+
 );
