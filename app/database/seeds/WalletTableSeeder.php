0c5dceab38dfd42f5b86253b803eab42bfd76379
Oisin <oisin@blocktrail.com>
2015-01-21 12:14:36 +0100

    check if wallet is saved successfully

--- app/database/seeds/WalletTableSeeder.php
+++ app/database/seeds/WalletTableSeeder.php
@@ -8,7 +8,11 @@ class WalletTableSeeder extends Seeder {
         //DB::table('wallets')->delete();
 
         $user = User::where('fname', 'joe')->first();
-        Wallet::create(array('identity' => str_random(40), 'name' => 'my wallet', 'pass' => str_random(6), 'user_id' => $user->id));
+        $wallet = new Wallet(array('identity' => str_random(40), 'name' => 'my wallet', 'pass' => str_random(6), 'user_id' => $user->id));
+        if(!$wallet->save()) {
+            $errors = Session::get('wallet-error');
+            echo "\n unable to create wallet: {$errors}\n";
+        }
     }
 
 }
